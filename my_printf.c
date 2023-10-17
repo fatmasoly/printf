9b35c16bb96f72827c80cd2cd73d3b6a060439d9
fatma <fatmacivil@hotmail.com>
2023-10-17 09:46:35 -0700

    try100

--- my_printf.c
+++ my_printf.c
@@ -48,27 +48,39 @@ return (len);
  */
 int printf_int(va_list ptr)
 {
-int num;
-int len = 0;
+int num, temp, j;
+int len = 0, is_neg = 0;
+char buff[12];
 num = va_arg(ptr, int);
 if (num < 0)
 {
 _putchar('-');
 num = -num;
-len++;
+is_neg = 1;
 }
 if (num == 0)
 {
 _putchar('0');
-return (1);
-}
-while (num > 0)
-{
-_putchar((num % 10) + '0');
-num /= 10;
 len++;
+temp = num;
 }
-return (len);
+while (temp != 0)
+{
+temp /= 10;
+len++;
+j = len - 1;
+}
+while (j >= 0)
+{
+buff[j] = (num % 10) + '0';
+num /= 10;
+j--;
+}
+for (j = 0 ; j < len ; j++)
+{
+_putchar(buff[j]);
+}
+return (len + is_neg);
 }
 /**
  * _printf - Custom printf function that processes format specifiers.
